d70237c59c9dcc067f480f654f1e6a63c9d59afe
Wai Yee <wwaiyyee@gmail.com>
2026-09-12 20:35:05 +0800

    remove useless components

--- packages/nextjs/components/ScaffoldHbarAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldHbarAppWithProviders.tsx
@@ -8,7 +8,6 @@ import { WagmiProvider } from "wagmi";
 import { Footer } from "~~/components/Footer";
 import { Header } from "~~/components/Header";
 import { LocalChainErrorBanner } from "~~/components/LocalChainErrorBanner";
-import { WalletDebugInfo } from "~~/components/WalletDebugInfo";
 import { HederaWalletConnectProvider } from "~~/services/web3/hederaWalletConnect";
 import { wagmiConfig } from "~~/services/web3/wagmiConfig";
 
@@ -22,7 +21,6 @@ const ScaffoldHbarApp = ({ children }: { children: React.ReactNode }) => {
         <Footer />
       </div>
       <Toaster />
-      <WalletDebugInfo />
     </>
   );
 };
